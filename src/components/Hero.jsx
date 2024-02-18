bdc3f1b0e3ee933c7454a0b3b83c1b4e7f25b9fa
ClintonW <clintonwambugu@gmail.com>
2024-02-18 14:59:55 +0300

    Add why us content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import RequestInviteButton from "./RequestInviteButton";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className=" border flex h-96">
+      <div className="pl-28 pt-14">
+        <h2
+          className="text-primary-dark-blue font-semibold"
+          style={{ fontSize: "44px" }}
+        >
+          Next generation digital banking
+        </h2>
+        <p className="text-neutral-grayish-blue py-8">
+          Take your financial life online. Your Easybank account will be a
+          one-stop-shop for spending, saving, budgeting, investing, and much
+          more.
+        </p>
+        <RequestInviteButton />
+      </div>
+      <div className="border basis-3/4 bg-[url('./bg-intro-desktop.svg')] bg-right bg-cover"></div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
-
 function WhyUs() {
   return (
-    <div>WhyUs</div>
-  )
+    <div className="bg-neutral-light-grayish-blue px-28">
+      <div className=" pt-20 ">
+        <h3
+          className="text-primary-dark-blue font-bold "
+          style={{ fontSize: "36px" }}
+        >
+          Why choose Easybank?
+        </h3>
+        <p className="text-neutral-grayish-blue my-8 w-3/5">
+          We leverage Open Banking to turn your bank account into your financial
+          hub. Control your finances like never before.
+        </p>
+      </div>
+      <div className="flex justify-between gap-4 mt-20">
+        <div>
+          <img src="./icon-online.svg" alt="" />
+          <h4 className="py-8">Online Banking</h4>
+          <p>
+            Our mordern web and mobile applications allow you to keep track of
+            your finances wherever you are in the world .
+          </p>
+        </div>
+        <div>
+          <img src="./icon-online.svg" alt="" />
+          <h4 className="py-8">Simple Budgeting</h4>
+          <p>
+            see exactly where your money goes each month. Recieve notifications
+            when you're close to hitting your limits.
+          </p>
+        </div>
+        <div>
+          <img src="./icon-online.svg" alt="" />
+          <h4 className="py-8">Fast Onboarding</h4>
+          <p>
+            We don't do branches. Open your account in minutes online and start
+            taking control of your finances right away.
+          </p>
+        </div>
+        <div>
+          <img src="./icon-online.svg" alt="" />
+          <h4 className="py-8">Open API</h4>
+          <p>
+            Manage your savings, investments, pension, and much more from one
+            account. Tracking you money has never been easier
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default WhyUs
+export default WhyUs;
