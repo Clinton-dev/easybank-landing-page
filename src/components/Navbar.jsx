aaef8bc0f0e0677083fdedd6612e02801c56cb47
ClintonW <clintonwambugu@gmail.com>
2024-02-18 21:25:50 +0300

    Add mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,37 @@ import RequestInviteButton from "./RequestInviteButton";
 
 function Navbar() {
   return (
-    <nav className="flex justify-around py-8 items-baseline bg-neutral-white ">
-      <div>
-        <img src="./logo.svg" alt="" />
-      </div>
-      <div>
-        <ul className="flex gap-8 text-neutral-grayish-blue">
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Blog</li>
-          <li>Careers</li>
-        </ul>
-      </div>
-      <div>
-        <RequestInviteButton />
-      </div>
-    </nav>
+    <>
+      {/* desktop */}
+      <nav className="hidden md:flex justify-around py-8 items-baseline bg-neutral-white ">
+        <div>
+          <img src="./logo.svg" alt="" />
+        </div>
+        <div>
+          <ul className="flex gap-8 text-neutral-grayish-blue">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Careers</li>
+          </ul>
+        </div>
+        <div>
+          <RequestInviteButton />
+        </div>
+      </nav>
+      {/* mobile */}
+      <nav>
+        <div>
+          <img src="./logo.svg" alt="" />
+        </div>
+        <div>
+          <button>
+            <img src="./icon-hamburger.svg" alt="" />
+          </button>
+        </div>
+      </nav>
+    </>
   );
 }
 
