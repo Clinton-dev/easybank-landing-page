6973b162777f76079c903795f411ce5b0d3405f1
clinton-dev <clintonwambugu@gmail.com>
2024-02-19 15:52:21 +0300

    Add mobile nav menuss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import RequestInviteButton from "./RequestInviteButton";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <>
       {/* desktop */}
@@ -27,11 +33,27 @@ function Navbar() {
           <img src="./logo.svg" alt="" />
         </div>
         <div>
-          <button>
-            <img src="./icon-hamburger.svg" alt="" />
+          <button onClick={toggleMenu}>
+            <img
+              src={isOpen ? "./icon-close.svg" : "./icon-hamburger.svg"}
+              alt=""
+            />
           </button>
         </div>
       </nav>
+      {/* mobile menu items */}
+
+      {isOpen && (
+        <div className="absolute top-[20%] right-[8%] text-primary-dark-blue bg-neutral-white md:hidden py-8 border rounded w-5/6 drop-shadow-2xl">
+          <ul className="flex flex-col gap-8 text-center">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Careers</li>
+          </ul>
+        </div>
+      )}
     </>
   );
 }
