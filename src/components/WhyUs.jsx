7691439e049698335334e303809d563cded878f1
clinton-dev <clintonwambugu@gmail.com>
2024-02-19 14:32:17 +0300

    Make why us section responsive

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -1,6 +1,6 @@
 function WhyUs() {
   return (
-    <div className="bg-neutral-light-grayish-blue px-28 pb-16">
+    <div className="bg-neutral-light-grayish-blue text-center md:text-left px-4 md:px-28 pb-20 md:pb-16">
       <div className=" pt-20 ">
         <h3
           className="text-primary-dark-blue font-bold "
@@ -8,14 +8,16 @@ function WhyUs() {
         >
           Why choose Easybank?
         </h3>
-        <p className="text-neutral-grayish-blue my-8 w-3/5">
+        <p className="text-neutral-grayish-blue my-8 md:w-3/5">
           We leverage Open Banking to turn your bank account into your financial
           hub. Control your finances like never before.
         </p>
       </div>
-      <div className="flex justify-between gap-4 mt-20">
+      <div className="flex justify-between flex-col md:flex-row gap-4 mt-20">
         <div>
-          <img src="./icon-online.svg" alt="" />
+          <div className="flex justify-center md:justify-start">
+            <img src="./icon-online.svg" alt="" />
+          </div>
           <h4 className="py-8">Online Banking</h4>
           <p>
             Our mordern web and mobile applications allow you to keep track of
@@ -23,7 +25,9 @@ function WhyUs() {
           </p>
         </div>
         <div>
-          <img src="./icon-online.svg" alt="" />
+          <div className="flex justify-center md:justify-start">
+            <img src="./icon-online.svg" alt="" />
+          </div>
           <h4 className="py-8">Simple Budgeting</h4>
           <p>
             see exactly where your money goes each month. Recieve notifications
@@ -31,7 +35,9 @@ function WhyUs() {
           </p>
         </div>
         <div>
-          <img src="./icon-online.svg" alt="" />
+          <div className="flex justify-center md:justify-start">
+            <img src="./icon-online.svg" alt="" />
+          </div>
           <h4 className="py-8">Fast Onboarding</h4>
           <p>
             We don't do branches. Open your account in minutes online and start
@@ -39,7 +45,9 @@ function WhyUs() {
           </p>
         </div>
         <div>
-          <img src="./icon-online.svg" alt="" />
+          <div className="flex justify-center md:justify-start">
+            <img src="./icon-online.svg" alt="" />
+          </div>
           <h4 className="py-8">Open API</h4>
           <p>
             Manage your savings, investments, pension, and much more from one
